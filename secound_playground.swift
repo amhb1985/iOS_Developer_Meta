75881b230770abb09fa65876987391026c1c2fa0
amhb1985 <amirhabibi219@gmail.com>
2024-02-04 16:14:00 +0100

    explain

--- secound_playground.swift
+++ secound_playground.swift
@@ -1,12 +1,21 @@
+
+
+//the project is the calculate the Weather
 import UIKit
 
-
+// we can use one variable with let day in "String"
+// and we can add another as Float or Integer
 let day = "Monday"
 let dailyTemprature = 89
 
 /'for the mix the two Variable we can use "\">>"7+shfit+option" inside of ()'/
 
+// for the 1'st we can use the day
+//  print("txt   \(var)      txt.")
+
 print("today is\(day). Rise and Shine!")
+
+//again we can show with "  txt   \()  txt   \()   txt    "
 print("the teperator on \(day) is \(dailyTemprature) F.")
 
 /'definition the Tempratur as Varible'/
