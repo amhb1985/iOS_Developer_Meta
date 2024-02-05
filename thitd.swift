74be406b74100c92e9b83a56004454f7503369d4
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-05 06:04:11 +0000

    Logical_symbole_boolan

---
+++ thitd.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+// Boolan true False
+// its look like the python
+
+
+//symbole for logically :
+
+/*
+==      equal   
+>       greater than
+>=      grater or equal
+<       smaller than
+<=      smaller or equal
+
+!=      not eqal
+
+
+*/
+
+
+
+
+
+
