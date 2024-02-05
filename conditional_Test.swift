044121eec90fc508d7fd59f22878ad38ab179571
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-05 07:25:01 +0000

    con_language

---
+++ conditional_Test.swift
@@ -0,0 +1,14 @@
+
+//we can set three language to default language:
+let language = "spanish"
+
+if language == "english" {
+    print("the defaul language is English!")
+    
+}else if language == "deutsch"{
+    print("Aktuelle Sprache ist Deutsch!")
+}else if language == "spanish"{
+    print("the language is Spanish!")
+}else{
+    print("the default language is NOT set!")
+}
