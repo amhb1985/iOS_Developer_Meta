d279468eb9277e863c1bd802ca7782856df4f938
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-05 06:48:53 +0000

    else_cond_waterBuiling

--- conditional.swift
+++ conditional.swift
@@ -6,3 +6,13 @@ if waterTemp == 0 {
     print("the water is freezing now!")
     
 }
+let watertemp = 92 
+
+if watertemp >= 100{
+    print("water is builing now!")
+    else {
+        print("the water is not builing!")
+    }
+
+
+}
