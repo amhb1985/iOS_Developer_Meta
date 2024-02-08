d3e83d8d13116c4b13264bd58d07498080b99c8f
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-08 14:13:17 +0000

    break_test

--- nasted02.swift
+++ nasted02.swift
@@ -5,6 +5,10 @@ while(month < 4){
     print("Month: \(month)")
     
     for i in 1...4{
+         //Braek test
+        if i == 3 {
+            break
+        }
         print("count: \(i)")
     }
     
