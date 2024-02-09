29b1380ad0fd9af6df7cb78f041ee8778365aaf5
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-09 14:56:56 +0000

    making_tuple_list

---
+++ Prgramming_Fundament/Second_week/Tuples.swift
@@ -0,0 +1,17 @@
+//: [Previous](@previous)
+
+import Foundation
+
+//making the new List as VegArray for Resturant:
+//var vegArray :[Any] = ["Carrot", 3, true]
+
+// making the same List as vegtable BUT in tuple!
+var vegtable = ("Carrot", 3, true)
+
+// making another tuple for list of vegtable:
+var (vegType, quantity, inStock) = vegtable
+
+//we can see or set or choose the name of article in our tuple List:
+print(vegType)
+
+
