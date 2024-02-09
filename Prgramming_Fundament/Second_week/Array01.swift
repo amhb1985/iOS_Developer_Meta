6b05b23f320063e12019ceb056fc87a08651adc5
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-09 14:27:02 +0000

    chnaging_editing_explaining

--- Prgramming_Fundament/Second_week/Array01.swift
+++ Prgramming_Fundament/Second_week/Array01.swift
@@ -1,27 +1,34 @@
 
 //exersise in Swift:
+//: [Previous](@previous)
 
-//1.
-//2.
-//3.
-//4.
-//5.
-//6.
-//7.
+import Foundation
 
+//test Array in swift:
 
+//1: Create an empty array
 var levelScores: [Int] = []
+
+//2: Use the count property to determine if the game has started
 if levelScores.count == 0 {
   print("Start playing the game!")
 }
+
+//3: Add the first level’s score
 let firstLevelScore = 10
 levelScores.append(firstLevelScore)
 print("The first level's score is \(levelScores[0]).")
+
+//4.Step 4: Add the bonus score to the first level’s score
 let levelBonusScore = 40
 levelScores[0] += levelBonusScore
 print("The first level's score is \(levelScores[0]).")
+
+//5.Step 5: Add more values to the array
 let freeLevelScores = [20, 30]
 levelScores += freeLevelScores
+
+//6.Check if free version of the game has been played
 let freeLevels = 3
 if levelScores.count == freeLevels {
   print(
@@ -29,4 +36,5 @@ if levelScores.count == freeLevels {
   )
   levelScores = []
   print("Game restarted!")
+}
 }
