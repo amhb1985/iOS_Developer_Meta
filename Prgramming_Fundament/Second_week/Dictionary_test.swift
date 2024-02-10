d91a34fe0167e809b0ffa0d8927dbefbc8251914
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-10 07:42:10 +0000

    test_Dic

---
+++ Prgramming_Fundament/Second_week/Dictionary_test.swift
@@ -0,0 +1,19 @@
+//: [Previous](@previous)
+
+import Foundation
+
+
+//Scenario
+
+/*
+ Weather apps are handy tools that allow users to review the weather forecast for a specific timeline. For this activity, you will use dictionaries to store the daily temperatures of a specific week.
+*/
+
+
+//1: Create a dictionary
+//Creating an empty dictionary and explicitly setting its type.
+
+var weeklyTemperatures: [String: Int] = [:]
+
+
+
