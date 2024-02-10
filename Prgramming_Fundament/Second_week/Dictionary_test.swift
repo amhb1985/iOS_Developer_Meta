ec8e7dba119cda150092defbd8d4ac97c4d1f213
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-10 07:54:34 +0000

    edit_leer_in_Dic

--- Prgramming_Fundament/Second_week/Dictionary_test.swift
+++ Prgramming_Fundament/Second_week/Dictionary_test.swift
@@ -1,8 +1,5 @@
 //: [Previous](@previous)
 
-import Foundation
-
-
 //Scenario
 
 /*
@@ -12,16 +9,29 @@ import Foundation
 
 //1: Create a dictionary
 //Creating an empty dictionary and explicitly setting its type.
+
 var weeklyTemperatures: [String: Int] = [:]
+
 
 //2: Set the daily temperatures in the dictionary
 weeklyTemperatures = ["Monday": 70, "Tuesday": 75, "Wednesday": 80, "Thursday": 85, "Friday": 90, "Saturday": 95]
 
+
 //3: Update and evaluate the first day’s temperature
-weeklyTemperatures["Monday"]! += 20print("The temperature on Monday is \(weeklyTemperatures["Monday"]!)°F.")
+weeklyTemperatures["Monday"]! += 20
+print("The temperature on Monday is \(weeklyTemperatures["Monday"]!)°F.")
 
 //4:  Add a new key-value pair:
-if let temperature = weeklyTemperatures["Sunday"] {  print("The temperature on Sunday is \(temperature)°F.")} else {  weeklyTemperatures["Sunday"] = 100  print("The temperature on Sunday is \(weeklyTemperatures["Sunday"]!)°F.")}
+if let temperature = weeklyTemperatures["Sunday"] {
+    print("The temperature on Sunday is \(temperature)°F.")
+} else {
+    weeklyTemperatures["Sunday"] = 100
+    print("The temperature on Sunday is \(weeklyTemperatures["Sunday"]!)°F.")
+}
 
-//5: Reset the dictionary for a new week: 
-if weeklyTemperatures.count == 7 {  print("You have access to the weather forecast of the whole week.")  weeklyTemperatures = [:]  print("Reset weekly temperatures for next week!")}
+//5: Reset the dictionary for a new week:
+if weeklyTemperatures.count == 7 {
+    print("You have access to the weather forecast of the whole week.")
+    weeklyTemperatures = [:]
+    print("Reset weekly temperatures for next week!")
+}
