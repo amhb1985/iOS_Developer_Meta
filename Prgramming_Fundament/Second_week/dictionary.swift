927b9e725be6b1e95acabdeac5025d2f0ad602a6
Amir Habibi <139058609+amhb1985@users.noreply.github.com>
2024-02-10 07:21:28 +0000

    Dictioanry_file

---
+++ Prgramming_Fundament/Second_week/dictionary.swift
@@ -0,0 +1,23 @@
+
+import Foundation
+
+
+//for a Dictionary we must: how we can creat and store
+// 1.create a "var Dic :"
+//  2. define "Dictionary<String, Int> =" (we can set another Type of data)
+//    3. explain Dictionary inside of " ["a" = 100, "b"= 200] "
+var travelMiles : Dictionary<String, Int> = ["Richie": 500, "Pter": 400, "Amy" : 800]
+
+//thesome function in Dictionary:
+//1. count:
+let itemCount = travelMiles.count
+
+
+//How we can create an empty Dictinary:
+// like above but we must set at First " = "instead of : and at end "()"
+var myDictionary = Dictionary<String, Int> ()
+
+
+//
+
+
